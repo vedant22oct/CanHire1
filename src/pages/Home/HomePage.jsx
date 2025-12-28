86bdddae656cb970bf4c777da51dc0b83e09e3dd
vedant22oct <dp@Mac.lan>
2025-12-28 15:04:16 -0600

    initial commit

---
+++ src/pages/Home/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Counter from '.../components/Counter'
+
+function HomePage() {
+  return (
+    <div>
+        <h1 className='page-title'>This is my Homepage!</h1>
+        <Counter />
+    </div>
+  )
+}
+
+export default HomePage
