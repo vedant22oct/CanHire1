e248cee0ff4a2b8c970b92309a501ffd084c7723
vedant22oct <vedant22oct@gmail.com>
2026-01-04 21:54:14 -0600

    modfied the latest code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,28 +9,18 @@ function Header() {
         <header>
             <ul className="nav-list">
                 <li><NavLink to="/">Home</NavLink></li>
-<<<<<<< HEAD
-                <li><NavLink to="/joblist">ApplyJob</NavLink></li>
-                <li><NavLink to="/signin">Candidate Signin</NavLink></li>
-                <li><NavLink to="/techinterview">TechInterview</NavLink></li>
-                <li><NavLink to="/techinterview">Location</NavLink></li>
-                <li><NavLink to="/techinterview">Equipment</NavLink></li>
-=======
-                <li><NavLink to="/products">Products</NavLink></li>
->>>>>>> 8d6fac1b39faa877845963d21edd5a9b2f5c9470
                 <li><NavLink to="/users">Users</NavLink></li>
                 <li><Link onClick={() => dispatch(logout())}>Logout</Link></li>
             </ul>
         </header>
     )
-<<<<<<< HEAD
     //test change
 }
 //<li><NavLink to="/products">Products</NavLink></li>
-                
+   /*<li><NavLink to="/joblist">ApplyJob</NavLink></li>
+                <li><NavLink to="/signin">Candidate Signin</NavLink></li>
+                <li><NavLink to="/techinterview">TechInterview</NavLink></li>
+                <li><NavLink to="/techinterview">Location</NavLink></li>
+                <li><NavLink to="/techinterview">Equipment</NavLink></li>*/
+                             
 export default Header;
-=======
-}
-
-export default Header
->>>>>>> 8d6fac1b39faa877845963d21edd5a9b2f5c9470
