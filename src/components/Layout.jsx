0b5f347ba32b91e61389e674142b3958abd41ce5
Butchaiah <butchaiah41460@gmail.com>
2026-01-06 21:49:10 -0500

    testing

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,12 @@
 import Header from "./Header";
 
+
 function Layout(props) {
     return (
         <main>
             <Header />
             <section>
-                {/* <aside>side bar</aside> */}
+                {}
                 <div className="page-content" style={{padding: 20}}>
                     {props.children}
                 </div>
