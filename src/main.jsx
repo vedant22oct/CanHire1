28a751d9ce7854b728a51c6998ce86efcbf3328f
pearlpandz <12746886+pearlpandz@users.noreply.github.com>
2026-01-07 07:53:20 +0530

    revert the changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux';
-import { store } from './redux/store.js';
+import store from './redux/store.js';
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  // </StrictMode>,
+  <Provider store={store}>
+    <App />
+  </Provider>
+  //</StrictMode>,
 )
